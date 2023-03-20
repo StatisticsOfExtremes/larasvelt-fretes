795c2a21debb2f3937cae7dfac67414be45296bc
alyssondasm <alyssondasm@gmail.com>
2023-03-20 20:02:48 -0300

    maybe uma tabela

---
+++ src/routes/todos/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+
+</script>
+
+<div class="container mx-auto items-center p-8 border-cyan-100 border-t">
+  <label class="flex">Filtrar</label>
+  <input class="flex mb-4 rounded-md pl-1 pr-1 bg-slate-400 text-left" type="search">
+  <table class="w-auto max-w-5xl table-auto rounded-md border-solid bg-slate-200">
+    <thead class="bg-gray-100">
+      <tr class="">
+        <th class="text-left table-column w-10"> </th>
+        <th class="text-left">Song</th>
+        <th class="text-left w-36">Artist</th>
+        <th class="text-left w-12">Year</th>
+      </tr>
+    </thead>
+    <tbody class="">
+      <tr class="table-row border-t hover:bg-slate-100">
+        <td class=" text-center table-cell"> <input type="checkbox"></td>
+        <td class="table-cell w-150">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+        <td class="table-cell">Malcolm Lockyer</td>
+        <td class="table-cell">1961</td>
+      </tr>
+      <tr class="border-t hover:bg-slate-100">
+        <td class="text-center"><input type="checkbox"> </td>
+        <td class="left-3 table-cell">Witchy Woman</td>
+        <td class="table-cell">The Eagles</td>
+        <td class="table-cell">1972</td>
+      </tr>
+      <tr class="border-t hover:bg-slate-100">
+        <td class="table-cell text-center"> <input type="checkbox"> </td>
+        <td class="table-cell">Shining Star</td>
+        <td class="table-cell">Earth, Wind, and Fire</td>
+        <td class="table-cell">1975</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
