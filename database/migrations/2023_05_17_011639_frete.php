da49da2e7662b05bf7c133a6087d44d8610a6a64
Alysson da Silva <alyssondasm@gmail.com>
2023-05-16 22:49:28 -0300

    migration dos fretes, provavelmente falta informações aqui

--- database/migrations/2023_05_17_011639_frete.php
+++ database/migrations/2023_05_17_011639_frete.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('fretes', function (Blueprint $table) {
             $table->increments('id');
             $table->date('data_carregamento')->nullable();
+            $table->enum('status', ['carregando', 'em andamento', 'finalizado']);
             $table->foreignId('id_carreteiro');
             $table->foreignId('id_nota');
             $table->decimal('frete_carreteiro', 8, 2)->nullable();
